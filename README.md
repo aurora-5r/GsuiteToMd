1e31cee383def008ab4441885555ad264623033d
Laurent Maumet <laurent.maumet@gmail.com>
2021-01-06 20:43:10 +0000

    date

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# gsuitePython
+# Gsuite to Markdown.
+
+Some tools to generate markdown files from Google Suite.
--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='GsuiteToMd',
     version='0.1',
-    packages=['gstomd', 'gstomd.test'],
     author='Laurent Maumet',
     author_email='laurent@aurora-5r.fr',
     url='https://github.com/laurentmau/GsuiteToMd',
-    license='LICENSE',
-    description='',
-    long_description=open('README.md').read(),
+    description='Tools to convert gsuite Documents to markdown',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License : : OSI Approved : : GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    python_requires='>=3.6',
+
 )
